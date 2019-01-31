bad6676ac49e8f21c488d1e3e8863db91ae870dd
williamabreu <williamsantos_00@hotmail.com>
2019-01-31 10:53:39 -0200

    Organizada classe mqtt

--- app/src/main/java/br/ufla/lcd/mamuterastreador/MQTT.java
+++ app/src/main/java/br/ufla/lcd/mamuterastreador/MQTT.java
@@ -4,115 +4,53 @@ import android.content.Context;
 import android.util.Log;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.UnsupportedEncodingException;
+
+/* https://www.hivemq.com/blog/mqtt-client-library-enyclopedia-paho-android-service */
+
 public class MQTT {
-    private MqttAndroidClient mqttAndroidClient;
+    private final String clientId;
+    private final MqttAndroidClient mqttClient;
+    private final MqttConnectOptions options;
 
-    private final String serverUri = "tcp://mamute.lcd.ufla.br:443";
+    public MQTT(Context context) {
+        clientId = MqttClient.generateClientId();
+        options = new MqttConnectOptions();
+        mqttClient = new MqttAndroidClient(context, "tcp://turing.lcd.ufla.br:6064", clientId);
+        options.setUserName("MMHdDK8sUsVWe7zBEahZwX5E");
+        options.setPassword("rfvnWGHbQNFF7M92NjQmxz99".toCharArray());
+    }
 
-    private final String clientId = "ExampleAndroidClient";
-    private final String topic = "AUG-0000/data";
-
-    private final String username = "MMHdDK8sUsVWe7zBEahZwX5E";
-    private final String password = "rfvnWGHbQNFF7M92NjQmxz99";
-
-    //public MqttMessage message = new MqttMessage("{\"latitude\":0.0,\"longitude\":0.0}".getBytes());
-
-
-    MQTT(Context context) {
-        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
-        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+    public void connect() throws MqttException {
+        IMqttToken token = mqttClient.connect(options);
+        token.setActionCallback(new IMqttActionListener() {
             @Override
-            public void connectComplete(boolean b, String s) {
-                Log.w("mqtt", s);
+            public void onSuccess(IMqttToken asyncActionToken) {
+                // We are connected
+                Log.d("TAG", "onSuccess");
             }
 
             @Override
-            public void connectionLost(Throwable throwable) {
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-                Log.w("Mqtt", mqttMessage.toString());
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                // Something went wrong e.g. connection timeout or firewall problems
+                Log.d("TAG", "onFailure");
 
             }
         });
-        connect();
     }
 
-    public void setCallback(MqttCallbackExtended callback) {
-        mqttAndroidClient.setCallback(callback);
-    }
-
-    private void connect() {
-        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setAutomaticReconnect(true);
-        mqttConnectOptions.setCleanSession(false);
-        mqttConnectOptions.setUserName(username);
-        mqttConnectOptions.setPassword(password.toCharArray());
-
-
-        Log.d("MQTT>connect", "connectou");
-
-        try {
-
-            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-
-                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
-                    disconnectedBufferOptions.setBufferEnabled(true);
-                    disconnectedBufferOptions.setBufferSize(100);
-                    disconnectedBufferOptions.setPersistBuffer(false);
-                    disconnectedBufferOptions.setDeleteOldestMessages(false);
-                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
-                    publishMessage("HELLOLWLLKLLKADSJLAS QWE");
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Log.w("Mqtt", "Failed to connect to: " + serverUri + exception.toString());
-                }
-            });
-
-
-        } catch (MqttException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void publishMessage(String payload) {
-
-
-        MqttMessage message;
-        message = new MqttMessage(payload.getBytes());
-
-        try
-
-        {
-            mqttAndroidClient.publish(topic, message);
-            Log.i("mqtt", "message published");
-
-        } catch (
-                MqttException ex)
-
-        {
-            System.err.println("Exceptionst subscribing");
-            ex.printStackTrace();
-        }
+    public void publish(String topic, String data) throws MqttException, UnsupportedEncodingException {
+        byte[] encodedPayload;
+        encodedPayload = data.getBytes("UTF-8");
+        MqttMessage message = new MqttMessage(encodedPayload);
+        mqttClient.publish(topic, message);
     }
 }
 
